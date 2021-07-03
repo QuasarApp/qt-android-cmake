b2a1b3d87bf114694961a4f03caf5925303e4e0b
EndrII <EndrIIMail@gmail.com>
2021-07-03 15:01:19 +0300

    fix build

--- AddQtAndroidApk.cmake
+++ AddQtAndroidApk.cmake
@@ -16,12 +16,13 @@ if(NOT ANDROID)
 endif()
 
 if(NOT QT_VERSION_MAJOR)
-    message(FATAL_ERROR "Trying to use the CMake Android package without the QT_VERSION_MAJOR variable. Please set QT_VERSION_MAJOR varibale to 5 or 6 value before include the AndroidApk")
+    message(WARNING "The QT_VERSION_MAJOR variable not definde by default use the qt5")
+    set(QT_VERSION_MAJOR 5)
 endif()
 
 # find the Qt root directory
 if(NOT Qt${QT_VERSION_MAJOR}Core_DIR)
-    find_package(Qt5Core REQUIRED)
+    find_package(Qt{QT_VERSION_MAJOR}Core REQUIRED)
 endif()
 get_filename_component(QT_ANDROID_QT_ROOT "${Qt${QT_VERSION_MAJOR}Core_DIR}/../../.." ABSOLUTE)
 message(STATUS "Found Qt for Android: ${QT_ANDROID_QT_ROOT}")
